80c4d8af16ac3eeab24a311fbc4a71bdaf277047
Akiyuki <akiyuki090@gmail.com>
2022-11-09 23:13:27 -0300

    finish

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import time
+import urllib
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.commom.keys import Keys
@@ -14,3 +15,13 @@ esperar achar um elemento da pg do wpp
 while len(nav.find_elements_by_id('side')) < 1:
     time.sleep(1)
 
+for i, mensagem in enumerate(contatos['COLUNA DO EXCEL PARA MENSAGEM']):
+    pessoa = contatos.loc[i, "COLUNA COM O NOME DA PESSOA"]
+    numero = contatos.loc[i, "COLUNA COM O NUMERO DA PESSOA"]
+    texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem}")
+    link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
+    nav.get(link)
+    while len(nav.find_elements_by_id("side")) < 1:
+        time.sleep(1)
+    nav.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]').send_keys(Keys.ENTER)
+    time.sleep(10)
